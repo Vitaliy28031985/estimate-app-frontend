a841c2acfd2a0b30fb041aaf90c83a4c0ad7ffd7
Vitaliy <94307584+Vitaliy28031985@users.noreply.github.com>
2024-01-20 11:41:13 +0200

    add workflow

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode basename='/estimate-app-frontend/'>
+  <React.StrictMode >
      <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename='/estimate-app-frontend/'>
     <App />
     </BrowserRouter>
     </Provider>
